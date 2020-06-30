8dd826ef5837142a71f2b0f86079b9ffd9979e78
gyoomi <gyoomi0709@foxmail.com>
2020-06-30 10:34:38 +0800

    feat(all): spring boot and  mybatis plus and druid 整合

---
+++ spring-boot-and-mybatis-plus-with-druid/src/main/java/com/gyoomi/mybatis/plus/druid/DruidMyBatisPlusApplication.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright © 2020, Glodon Digital Supplier BU.
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.gyoomi.mybatis.plus.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Leon
+ * @date 2020-06-30 10:29
+ */
+@SpringBootApplication
+public class DruidMyBatisPlusApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DruidMyBatisPlusApplication.class, args);
+	}
+
+}
---
+++ spring-boot-and-mybatis-plus-with-druid/src/main/java/com/gyoomi/mybatis/plus/druid/controller/TestController.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright © 2020, Glodon Digital Supplier BU.
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.gyoomi.mybatis.plus.druid.controller;
+
+import com.gyoomi.mybatis.plus.druid.dao.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Test
+ *
+ * @author Leon
+ * @date 2020-06-29 17:13
+ */
+@RestController
+public class TestController
+{
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@GetMapping("test")
+	public void test()
+	{
+		System.out.println(userMapper.selectById(839063639653627328L).toString());
+	}
+
+	@GetMapping("test2")
+	public void test2()
+	{
+		System.out.println(userMapper.selectList(null).toString());
+	}
+}
