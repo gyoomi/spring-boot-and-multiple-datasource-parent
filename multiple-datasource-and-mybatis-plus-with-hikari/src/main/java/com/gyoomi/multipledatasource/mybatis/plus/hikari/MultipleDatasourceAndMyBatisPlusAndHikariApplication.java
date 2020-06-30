c1ae4c4b7b50e8de8261676e43403c05fee44d3f
gyoomi <gyoomi0709@foxmail.com>
2020-06-30 17:10:24 +0800

    feat(all): spring boot and mybatis plus and hikari finished 简化配置

---
+++ multiple-datasource-and-mybatis-plus-with-hikari/src/main/java/com/gyoomi/multipledatasource/mybatis/plus/hikari/MultipleDatasourceAndMyBatisPlusAndHikariApplication.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright © 2020, Glodon Digital Supplier BU.
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.gyoomi.multipledatasource.mybatis.plus.hikari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Leon
+ * @date 2020-06-30 16:23
+ */
+@SpringBootApplication
+public class MultipleDatasourceAndMyBatisPlusAndHikariApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MultipleDatasourceAndMyBatisPlusAndHikariApplication.class, args);
+	}
+
+}
